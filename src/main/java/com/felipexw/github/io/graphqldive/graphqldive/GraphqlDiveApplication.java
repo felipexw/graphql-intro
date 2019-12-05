b4ef9d07a196b5a73df8643404aa54dc07725908
Felipe <felipe.appio.ext@sodexo.com>
2019-12-05 19:38:31 -0300

    first project

---
+++ src/main/java/com/felipexw/github/io/graphqldive/graphqldive/GraphqlDiveApplication.java
@@ -0,0 +1,13 @@
+package com.felipexw.github.io.graphqldive.graphqldive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlDiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlDiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felipexw/github/io/graphqldive/graphqldive/GraphqlDiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipexw.github.io.graphqldive.graphqldive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlDiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
